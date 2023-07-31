d01f461b898b9b16131ca26324fb25a92107950c
Gabby948 <teffa08-94@hotmail.com>
2023-07-31 13:51:53 -0500

    test enviroment adjust

---
+++ BQProject/test/app.test.tsx
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ClientInput from '../src/components/waiter/client';
+
+test('renders the ClientInput component correctly', () => {
+  render(<ClientInput />);
+  
+  const inputElement = screen.getByPlaceholderText('Nombre del cliente');
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('input element is empty initially', () => {
+  render(<ClientInput />);
+  
+  const inputElement = screen.getByPlaceholderText('Nombre del cliente');
+  expect(inputElement).toHaveValue('');
+});
+
+test('typing updates the input value', () => {
+  render(<ClientInput />);
+  
+  const inputElement = screen.getByPlaceholderText('Nombre del cliente');
+  const testValue = 'John Doe';
+
+  // Simulate typing the value
+  userEvent.type(inputElement, testValue);
+
+  expect(inputElement).toHaveValue(testValue);
+});
