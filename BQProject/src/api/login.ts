a239c9a33f6d44ea6abdc75192f0bc1e867e0de0
Clau2210 <claudiauriascaudillo@gmail.com>
2023-07-30 21:02:05 -0600

    Id de usuario

--- BQProject/src/api/login.ts
+++ BQProject/src/api/login.ts
@@ -2,20 +2,31 @@ import { instance } from './base.api'
 
 const endpoint = 'login'
 
-interface AuthResponse {
-  accessToken: string;
+interface User {
+  email: string;
+  role: string;
+  id: number;
 }
 
-export const login = async (email: string, password: string): Promise<string | null> => {
+interface AuthResponse {
+  accessToken: string;
+  user: User
+}
+
+export const login = async (email: string, password: string): Promise<AuthResponse | null> => {
   try {
     const response = await instance.post<AuthResponse>(endpoint, {
       'email': email,
       'password': password
     });
-    const { accessToken } = response.data;
+    const { accessToken, user } = response.data;
+    if (!user || !user.id) {
+      throw new Error('ID del usuario no está presente en la respuesta.');
+    }
     // Almacenar el token de acceso en localStorage
     localStorage.setItem('accessToken', accessToken);
-    return accessToken;
+    localStorage.setItem('userId', user.id.toString());
+    return { accessToken, user };
   } catch (error) {
     console.error('Error en la autenticación:', error);
     return null;
