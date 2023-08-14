e6c0e0321d904aa90f4a458d7f4a239c26cd73a0
Clau2210 <claudiauriascaudillo@gmail.com>
2023-08-13 22:30:53 -0600

    Delete y ProductsMenuManager

--- BQProject/src/App.tsx
+++ BQProject/src/App.tsx
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/login";
 import WaiterLunch from "./components/waiter/waiterLunch";
 import Kitchen from "./components/orders/kitchen";
 import Waiter from "./components/waiter/Waiter";
-import Staff from "./components/staff/staffList"
+import Staff from "./components/staff/staffList";
+import ProductsMenuManager from "./components/menu/ProductsMenuManager";
 
 
 const App: React.FC = () => {
+  const [deletedProductIds, setDeletedProductIds] = useState<number[]>([]);
+  const handleDeleteProduct = (productId: number) => {
+    setDeletedProductIds((prevIds) => [...prevIds, productId]);
+  };  
+
   return (
       <BrowserRouter>
         <Routes>
@@ -17,6 +24,10 @@ const App: React.FC = () => {
           <Route path="/orderKitchen" element={<Kitchen />} />
           <Route path="/waiter" element={<Waiter />} />
           <Route path="/staffList" element={<Staff />} />  
+          <Route path="/ProductsMenuManager" element={<ProductsMenuManager
+           deletedProductIds={deletedProductIds}
+           onDeleteProduct={handleDeleteProduct}
+          />}/>  
         </Routes>
       </BrowserRouter>
   );
